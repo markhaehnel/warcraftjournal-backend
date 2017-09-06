7bddba68accd3e9687b56d11f98ca8aaec6d041d
Mark Hähnel <hello@markhaehnel.de>
2017-09-06 15:43:45 +0200

    Moved mongoose plugin out of initialization

--- app/core/databases.js
+++ app/core/databases.js
@@ -1,20 +1,20 @@
 var applicationStorage = require('core/application-storage')
 var mongoose = require('mongoose')
 
+mongoose.plugin((schema) => {
+    schema.options.toJSON = {
+        transform (doc, ret) {
+            delete ret._id
+            delete ret.__v
+        }
+    }
+})
+
 module.exports.start = () => { this.startMongo() }
 
 module.exports.startMongo = async () => {
     return new Promise((resolve, reject) => {
         mongoose.Promise = global.Promise
-
-        mongoose.plugin((schema) => {
-            schema.options.toJSON = {
-                transform (doc, ret) {
-                    delete ret._id
-                    delete ret.__v
-                }
-            }
-        })
 
         applicationStorage.mongo = mongoose.connect(applicationStorage.config.database.mongo).connection
             .once('open', () => {
