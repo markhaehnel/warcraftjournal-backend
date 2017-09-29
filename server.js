871a143ea616ee31445227f5a80ecce1a2eb4dea
Mark Hähnel <hello@markhaehnel.de>
2017-09-29 14:58:39 +0200

    Added New Relic monitoring

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 // Set module root directory and define a custom require function
 require('app-module-path').addPath(`${__dirname}/app`)
 
+// Enable monitoring
+require('newrelic')
+
 require('core/bootstrap')()
