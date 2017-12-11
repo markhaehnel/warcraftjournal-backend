f7abde3c577ea1e9dd7b77fa6fe288f66fce69e6
Mark Hähnel <hello@markhaehnel.de>
2017-12-11 12:36:55 +0100

    Removed new relic

--- server.js
+++ server.js
@@ -1,9 +1,4 @@
 // Set module root directory and define a custom require function
 require('app-module-path').addPath(`${__dirname}/app`)
 
-// Enable monitoring
-if (process.env.NODE_ENV === 'production') {
-  require('newrelic')
-}
-
 require('core/bootstrap')()
