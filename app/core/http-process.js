4ad317317038377c5d83b044a467040d71f177bf
Mark Hähnel <hello@markhaehnel.de>
2017-12-11 14:00:49 +0100

    Fixed routes module paths

--- app/core/http-process.js
+++ app/core/http-process.js
@@ -19,9 +19,9 @@ module.exports.start = async () => {
   app.use(cors())
 
   // Initialize api
-  app.use('/mythicplus', require('route/mythicplus.js'))
-  app.use('/guild', require('route/guild.js'))
-  app.use('/token', require('route/token.js'))
+  app.use('/mythicplus', require('routes/mythicplus.js'))
+  app.use('/guild', require('routes/guild.js'))
+  app.use('/token', require('routes/token.js'))
 
   // Log all other requests and send 404
   app.use((req, res) => {
