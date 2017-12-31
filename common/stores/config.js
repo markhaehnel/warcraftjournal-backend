dfc537057f1f8599f98fa547f52ab26a13521c5b
Mark Hähnel <hello@markhaehnel.de>
2017-12-31 12:20:18 +0100

    Removed obsolete logger config

--- common/stores/config.js
+++ common/stores/config.js
@@ -2,10 +2,6 @@ const config = Object.freeze({
   port: process.env.PORT || 3000,
   mongo: process.env.MONGODB || 'mongodb://localhost/warcraftjournal',
   redis: process.env.REDIS || 'redis://localhost',
-  logger: {
-    folder: 'logs',
-    level: 'debug'
-  },
   battlenet: {
     clientID: process.env.BATTLENET_CLIENTID,
     clientSecret: process.env.BATTLENET_CLIENTSECRET
